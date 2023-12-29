9e95875199ac42b3a7a7535c94d41d46be7d72ae
KTersova <146125509+KTersova@users.noreply.github.com>
2023-12-29 13:53:52 +0100

    Update Question 2 - Bread and milk.sql

--- Question 2 - Bread and milk.sql
+++ Question 2 - Bread and milk.sql
@@ -4,13 +4,13 @@
 -- PRO SELECT NÍŽE JE JIŽ TŘEBA MÍT VYTVOŘENU PRIMÁRNÍ TRABULKU t_katerina_tersova_project_sql_primary_final - viz. Tables / SQL_script_complete
 
 SELECT Produkt,
-	   Rok,
-	   ROUND(AVG(Průměrná_mzda), 2) AS Průměrná_mzda,
-	   ROUND(AVG(Cena),2) AS Cena,
-       ROUND(AVG(Průměrná_mzda)/AVG(Cena),0) AS Zakoupitelné_množství
+	Rok,
+        ROUND(AVG(Průměrná_mzda), 2) AS Průměrná_mzda,
+        ROUND(AVG(Cena),2) AS Cena,
+        ROUND(AVG(Průměrná_mzda)/AVG(Cena),0) AS Zakoupitelné_množství
 FROM t_katerina_tersova_project_sql_primary_final
 WHERE Produkt IN ('Chléb konzumní kmínový','Mléko polotučné pasterované') 
-	  AND Rok IN ('2006','2018')
+	AND Rok IN ('2006','2018')
 GROUP BY 
 	Produkt,
 	Rok
